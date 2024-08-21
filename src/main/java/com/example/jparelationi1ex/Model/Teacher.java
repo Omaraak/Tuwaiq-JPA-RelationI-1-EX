49428ce5afaab7465f1347a4357d1b6e1ebd3a72
Omar Alkhuwaytim <84483586+Omaraak@users.noreply.github.com>
2024-08-21 17:19:18 +0300

    Add files via upload

---
+++ src/main/java/com/example/jparelationi1ex/Model/Teacher.java
@@ -0,0 +1,39 @@
+package com.example.jparelationi1ex.Model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotEmpty
+    @Size(min = 4, max = 10)
+    @Column(columnDefinition = "varchar(10) not null")
+    private String name;
+    @NotNull
+    @Positive
+    @Column(columnDefinition = "int not null")
+    private int age;
+    @NotEmpty
+    @Email
+    @Size(min = 7, max = 20)
+    @Column(columnDefinition = "varchar(20) not null")
+    private String email;
+    @NotNull
+    @Positive
+    @Column(columnDefinition = "int not null")
+    private int salary;
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "teacher")
+    @PrimaryKeyJoinColumn
+    private Address address;
+}
